ff356c685af1d65b77c450e5bb2766b2390cc75b
edi vidrascu <edividrascu@gmail.com>
2024-01-08 22:07:21 +0100

    prueba de btn BORRAR

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
-(function (x) {
-  return (function (y) {
-    console.log(x);
-  })(2);
-})(1);
+const toggleBtn = document.getElementById('toggleBtn');
+const body = document.body;
+const container = document.querySelector('.container');
+const containerEcosystem = document.querySelector('.container-ecosystem');
+// Verifica si hay una preferencia almacenada en localStorage
+if (localStorage.getItem('dark-mode') === 'enabled') {
+  body.classList.add('dark-mode');
+  container.classList.add('container-dark');
+  containerEcosystem.classList.add('ecosystem-dark');
+}
+
+toggleBtn.addEventListener('click', () => {
+  // Alternar clases y guardar preferencia en localStorage
+  body.classList.toggle('dark-mode');
+  container.classList.toggle('container-dark');
+  containerEcosystem.classList.toggle('ecosystem-dark');
+  
+  if (body.classList.contains('dark-mode')) {
+    localStorage.setItem('dark-mode', 'enabled');
+  } else {
+    localStorage.setItem('dark-mode', 'disabled');
+  }
+});
