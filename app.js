c01f88dbe5a224be38ed9b7d779bce86982d56fb
edi vidrascu <edividrascu@gmail.com>
2024-01-26 12:56:13 +0100

    FINISH

--- app.js
+++ app.js
@@ -1,51 +1,120 @@
+document.addEventListener("DOMContentLoaded", function () {
+  let darkModeToggle = document.querySelector(".input-dark");
+  let solIcon = document.querySelector(".sol-dark");
+  let lunaIcon = document.querySelector(".luna-dark");
+  let isDarkMode = localStorage.getItem("darkMode") === "true";
 
-document.addEventListener('DOMContentLoaded', function () {
-  let darkModeToggle = document.querySelector('.input-dark');
-  let solIcon = document.querySelector('.sol-dark');
-  let lunaIcon = document.querySelector('.luna-dark');
-
-  // Verifica si hay un estado almacenado en localStorage
-  let isDarkMode = localStorage.getItem('darkMode') === 'true';
-
-  // Aplica el estado almacenado (oscuro o claro)
   darkModeToggle.checked = isDarkMode;
   updateIcons(isDarkMode);
 
-  darkModeToggle.addEventListener('change', function () {
+  darkModeToggle.addEventListener("change", function () {
     let isChecked = darkModeToggle.checked;
 
-    // Almacena el estado actual en localStorage
-    localStorage.setItem('darkMode', isChecked);
+    localStorage.setItem("darkMode", isChecked);
 
-    // Actualiza los iconos según el estado del modo oscuro
     updateIcons(isChecked);
   });
 
   function updateIcons(isDarkMode) {
     if (isDarkMode) {
-      // Modifica el color del sol cuando está seleccionado
-      solIcon.style.fill = '';
-      // Modifica el color de la luna cuando está seleccionada
-      lunaIcon.style.fill = '#a881fc';
+      solIcon.style.fill = "";
+
+      lunaIcon.style.fill = "#a881fc";
     } else {
-      // Restaura el color del sol a su estado original
-      solIcon.style.fill = '#ffb237';
-      // Restaura el color de la luna a su estado original
-      lunaIcon.style.fill = '';
+      solIcon.style.fill = "#ffb237";
+
+      lunaIcon.style.fill = "";
     }
   }
 });
 
+
+// drop DOWN11111111111111111
 function showDropdown() {
-  document.getElementById('dropdownOptions').style.display = 'block';
+  document.getElementById("dropdownOptions").style.display = "block";
+  
 }
+let selectedLanguage = localStorage.getItem('selectedLanguage') || 'ES-ES';
+document.getElementById('selectedOption').innerText = selectedLanguage;
 
 function hideDropdown() {
   document.getElementById('dropdownOptions').style.display = 'none';
+  
+
+}
+function selectLanguage(languageCode) {
+  // Guardar la elección del usuario en localStorage
+  localStorage.setItem('selectedLanguage', languageCode);
+
+  // Configurar el elemento seleccionado con la nueva elección
+  document.getElementById('selectedOption').innerText = languageCode;
+  
+  // Ocultar el dropdown después de seleccionar una opción
+  hideDropdown();
 }
 
-function selectLanguage(languageCode) {
-  document.getElementById('selectedOption').innerText = languageCode;
-  hideDropdown(); // Oculta la lista después de seleccionar una opción
+
+// drop DOWN 22222222222222222222222
+
+function showDropdown2() {
+  document.getElementById("dropdownOptions.second").style.display = "block";
+
+  
 }
+let selectedLanguage2 = localStorage.getItem('selectedLanguage') || 'ES-ES';
+document.getElementById('selectedLanguage.second').innerText = selectedLanguage2;
+
+function hideDropdown2() {
+  document.getElementById('dropdownOptions.second').style.display = 'none';
+  
+
+}
+function selectLanguage(languageCode) {
+  // Guardar la elección del usuario en localStorage
+  localStorage.setItem('selectedLanguage', languageCode);
+
+  // Configurar el elemento seleccionado con la nueva elección
+  document.getElementById('selectedLanguage.second').innerText = languageCode;
+  
+  // Ocultar el dropdown después de seleccionar una opción
+  hideDropdown2();
+}
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  let darkModeToggle2 = document.querySelector("#inputDarkSecond");
+  let solIcon2 = document.querySelector("#solDarkSecond");
+  let lunaIcon2 = document.querySelector("#lunaDarkSecond");
+
+  if (darkModeToggle2) {
+    let isDarkMode = localStorage.getItem("darkmode2") === "true";
+
+    darkModeToggle2.checked = isDarkMode;
+    updateIcons(isDarkMode);
+
+    darkModeToggle2.addEventListener("change", function () {
+      let isChecked = darkModeToggle2.checked;
+
+      localStorage.setItem("darkmode2", isChecked);
+
+      updateIcons(isChecked);
+    });
+  } else {
+    console.error("Elemento darkModeToggle2 no encontrado.");
+  }
+
+  function updateIcons(isDarkMode) {
+    if (isDarkMode) {
+      solIcon2.style.fill = "";
+      lunaIcon2.style.fill = "#a881fc";
+    } else {
+      solIcon2.style.fill = "#ffb237";
+      lunaIcon2.style.fill = "";
+    }
+  }
+});
+
+
+
 
